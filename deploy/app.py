583ea5980ca5996fdc43f7a7e4de8a4ea448ce94
양준헌 <greyhawk16@BlackBird-ui-MacBookPro.local>
2024-03-02 08:31:18 +0900

    파일업로드 기능 추가, templates 및 uploads 폴더 생성

--- deploy/app.py
+++ deploy/app.py
@@ -1,40 +1,46 @@
 # 메인 flask 앱
 import os
 
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = './uploads'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = "uploads/"
 
 
 # 참고자료: https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
-@app.route('/')
+@app.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():    # 시작 화면, 파일 업로드
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No files detected')
             return redirect([request.url])
         
-        file = request.files['file']
+        f = request.files['file']
 
-        if file.filename == '':
+        if f.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file:
-            file_name = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-            return redirect(url_for)
+        if f:
+            file_name = f.filename
+            f.save(app.config['UPLOAD_FOLDER'] + file_name)
+            return render_template("upload_result.html", name=file_name)
+    else:
+        return render_template("index.html")
         
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+
+
+@app.route('/analysis_result', methods = ['GET'])
+def display_analysis_result():
+    return render_template('analysis_result.html')
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8088, debug=True)
