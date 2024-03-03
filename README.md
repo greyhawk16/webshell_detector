43fd8257786739abd384be9676f2f06c187e21c7
양준헌 <greyhawk16@BlackBird-ui-MacBookPro.local>
2024-03-03 20:37:11 +0900

    파일 업로드 시, 올린 파일 목록 즉각 확인 가능

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # webshell_detector
 파이썬을 이용한 웹쉘 파일 탐지기
 
+## 프레임워크 및 사용 언어
+- <img src="https://img.shields.io/badge/Framework-%23121011?style=for-the-badge"><img src="https://img.shields.io/badge/flask-000000?style=for-the-badge&logo=flask&logoColor=white">
+- <img src="https://img.shields.io/badge/Language-%23121011?style=for-the-badge"><img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white"> 
+
+## 비즈니스 로직
+<details>
+<summary>클릭해서 읽어주세요!</summary>
+
+1. 파일 업로드
+
+2. 업로드 한 파일들을 확인하는 화면으로 이동
+
+3. `Analyze` 버튼 누르기 
+
+4. 분석 결과를 CSV로 표시
+
+5. `Download CSV` 링크를 눌러서 분석 결과를 CSV 파일로 다운로드 가능
+
+</details>
+
+## 기타
 도커파일은 현재 제작 중이며, 아직 사용하실 수 없습니다.
--- deploy/app.py
+++ deploy/app.py
@@ -25,16 +25,18 @@ def upload_file():    # 시작 화면, 파일 업로드
                 file.save(app.config['UPLOAD_FOLDER'] + file_name)
                 # 파일 이름에 특수문자 포함 시, 서비스 거부
 
-            return render_template("upload_result.html", name=files)
+            files = os.listdir(app.config['UPLOAD_FOLDER'])
+            return render_template('uploaded_file_list.html', files=files)
+            # return render_template("upload_result.html", name=files)       # upload_result.html은 미사용
     else:
         return render_template("index.html")
 
 
-@app.route('/uploaded_file_list', methods = ['GET'])
-def uploaded_files_dashboard():
-    # files = os.listdir("./uploads")
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('uploaded_file_list.html', files=files)
+# @app.route('/uploaded_file_list', methods = ['GET'])
+# def uploaded_files_dashboard():
+#     # files = os.listdir("./uploads")
+#     files = os.listdir(app.config['UPLOAD_FOLDER'])
+#     return render_template('uploaded_file_list.html', files=files)
 
 
 @app.route("/analysis_result", methods = ['GET'])
