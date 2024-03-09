f645a086fa13dae86cbe63db88613b6c1db810da
양준헌 <greyhawk16@BlackStar-ui-MacBookPro.local>
2024-03-09 21:14:26 +0900

    Dockerfile, docker-compose 파일 수정, os.stat_result에서 st_birthtime 속성을 못찾는 오류 해결 필요

--- deploy/app.py
+++ deploy/app.py
@@ -60,4 +60,4 @@ def home():
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8088, debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
