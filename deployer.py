105c29e362c91f983e63931a11a7dd6b50bcf27c
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-04-17 18:47:10 +0900

    ✨ deployer.py 오타 수정
    API Key 저장시 `$`가 같이 들어가는 현상 해결

--- deployer.py
+++ deployer.py
@@ -5,7 +5,7 @@ CMD = sys.argv[1]
 if CMD == "deploy":
     API_KEY = sys.argv[2]
     f = open("./code/deploy/.ENV", "w")
-    f.write(f"VIRUSTOTAL_API_KEY='${API_KEY}'")
+    f.write(f"VIRUSTOTAL_API_KEY='{API_KEY}'")
     f.close()
     
     os.system("cd ./terraform; terraform init; terraform apply")
